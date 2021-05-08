c967a7cb4aaa9f7d493e8a4eed32f1985697667b
HuuTrieu <trieuphuhuu1999@gmail.com>
2021-05-08 11:32:23 +0700

    "add login controller"

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -4,6 +4,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.login.LoginRequest;
 import com.example.demo.dto.login.LoginResponse;
 import com.example.demo.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+  @Autowired
   LoginService loginService;
 
   @PostMapping("/login")
   public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
     LoginResponse response = new LoginResponse();
     try {
-      if()
+      if(loginService.login(request.getPhone(), request.getPassword())) {
+        response.setCode(1);
+        response.setDiscription("Login success");
+      }
+      else {
+        response.setCode(-1);
+        response.setDiscription("Sai phone hoac password");
+      }
     }
     catch (Exception e){
       response.setCode(-1);
@@ -36,5 +45,6 @@ public class LoginController {
   @PostMapping("/signup")
   public ResponseEntity<LoginResponse> signup(@RequestBody LoginRequest request){
 
+    return null;
   }
 }
--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Shipper;
 import com.example.demo.repository.ShipperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,14 @@ public class LoginService {
   ShipperRepository shipperRepository;
 
   public boolean login(String phone, String password){
-
+    try {
+      Shipper shipper = shipperRepository.getById(phone);
+      if(shipper.getPassword().equals(password)) return true;
+      else return false;
+    }
+    catch (Exception e){
+      e.printStackTrace();
+      return false;
+    }
   }
 }
