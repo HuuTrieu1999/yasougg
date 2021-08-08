60dbfa0caac2540eb46d6fbfffce50ed9fac1308
huutp <trieuphuhuu1999@gmail.com>
2021-08-08 17:33:27 +0700

    "Update"

--- src/main/java/com/paylinks/dto/MailRequest.java
+++ src/main/java/com/paylinks/dto/MailRequest.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class MailRequest {
     String reciever;
+    String subject;
     String content;
     String type;
     String authenToken;
--- src/main/java/com/paylinks/service/YasouService.java
+++ src/main/java/com/paylinks/service/YasouService.java
@@ -6,39 +6,63 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class YasouService {
 
-  Logger logger = LoggerFactory.getLogger(this.getClass());
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  @Autowired
-  private JavaMailSender javaMailSender;
+    @Autowired
+    private JavaMailSender javaMailSender;
 
-  public String send(MailRequest request){
-    try {
-      sendEmail();
-      logger.info("Send email success");
-    } catch (Exception e) {
-      e.printStackTrace();
-      return e.getMessage();
+    public String send(MailRequest request) {
+        try {
+            if (request.getType().equals("text")) {
+                sendEmail(request);
+            } else if (request.getType().equals("html")) {
+                sendHtmlEmail(request);
+            }
+            logger.info("Send email success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        logger.info("Send done");
+        return "Success";
     }
-    logger.info("Send done");
-    return "Success";
-  }
 
-  void sendEmail() {
+    void sendEmail(MailRequest request) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(request.getReciever());
+        msg.setSubject(request.getSubject());
+        msg.setText(request.getContent());
+        javaMailSender.send(msg);
+    }
 
-    SimpleMailMessage msg = new SimpleMailMessage();
-    msg.setTo("trieuphuhuu1999@gmail.com"); //, "2@yahoo.com");
+    public void sendHtmlEmail(MailRequest request) throws MessagingException {
 
-    msg.setSubject("Testing from Spring Boot CN 17:00");
-    msg.setText("Hello World \n Spring Boot Email");
+        MimeMessage message = javaMailSender.createMimeMessage();
 
-    javaMailSender.send(msg);
+        boolean multipart = true;
 
-  }
+        MimeMessageHelper helper = new MimeMessageHelper(message, multipart, "utf-8");
+
+        String htmlMsg = "<h3>Im testing send a HTML email</h3>"
+                + "<img src='http://www.apache.org/images/asf_logo_wide.gif'>";
+
+        message.setContent(htmlMsg, "text/html");
+
+        helper.setTo(request.getReciever());
+
+        helper.setSubject(request.getSubject());
+
+        javaMailSender.send(message);
+    }
 
 
 }
