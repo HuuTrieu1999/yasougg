1c1bf5fcb7fa4cfd94855d492f641b9ce5d6fece
huutp <trieuphuhuu1999@gmail.com>
2021-08-08 17:37:34 +0700

    "Update"

--- src/main/java/com/paylinks/service/YasouService.java
+++ src/main/java/com/paylinks/service/YasouService.java
@@ -52,10 +52,7 @@ public class YasouService {
 
         MimeMessageHelper helper = new MimeMessageHelper(message, multipart, "utf-8");
 
-        String htmlMsg = "<h3>Im testing send a HTML email</h3>"
-                + "<img src='http://www.apache.org/images/asf_logo_wide.gif'>";
-
-        message.setContent(htmlMsg, "text/html");
+        message.setContent(request.getContent(), "text/html");
 
         helper.setTo(request.getReciever());
 
